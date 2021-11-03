700600095d0ed390f78c0246b218fa564a538278
mr. whalen <michael.whalen@sitechhs.com>
2021-11-03 12:31:49 -0400

    day 1

---
+++ async/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  //options API
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      name: "Daniel",
+    };
+  },
+  beforeCreate: function () {
+    console.log(this.name);
+  },
+  created: function () {
+    console.log(this.name);
+  },
+};
+</script>
